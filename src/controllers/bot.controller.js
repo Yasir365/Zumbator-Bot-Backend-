dd145539f7fd22121698d7e3759968a053883b7b
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-23 16:03:38 +0500

    Update bot.controller.js

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
         const existingUser = await User.findOne({ telegram_id: user.telegram_id });
 
         if (existingUser) {
-            res.status(400).send({ success: false, message: 'User already exists' });
+            res.status(200).send({ success: true, data: existingUser });
             return;
         }
         const data = await user.save();
