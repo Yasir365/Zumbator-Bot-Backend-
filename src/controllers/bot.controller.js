f8bbe378248e239fbdaca4357c3426cfa4fa4325
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-10-30 11:44:03 +0500

    udpated

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -21,27 +21,44 @@ export const register = async (req, res) => {
     }
 }
 
-export const saveRefUser = async (req, res) => {
-    const { telegram_id, ref } = req.body;
+export const saveReferal = async (req, res) => {
+    const user = req.body;
 
     try {
-        const newUser = await User.findOne({ telegram_id: telegram_id });
-        const existingUser = await User.findOne({ telegram_id: ref });
+        const newUser = await User.findOneAndUpdate({ 'user.id': user.user.id });
+        const existingUser = await User.findOne({ 'user.id': user.referral_id });
         let result = {}
         if (newUser && existingUser) {
             if (!newUser.referral_id) {
-                newUser.referral_id = existingUser.telegram_id;
+                newUser.referral_id = user.referral_id;
                 const updatedNewUser = await newUser.save();
+                result = updatedNewUser;
             }
 
             const index = existingUser.invited_friends.findIndex((id) => id == newUser._id);
             if (index != -1) {
                 existingUser.invited_friends = [...existingUser.invited_friends, newUser._id];
-                result = await existingUser.save();
+                await existingUser.save();
             }
         }
 
         res.status(200).send({ success: true, data: result });
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
+}
+
+export const getReferal = async (req, res) => {
+    const user = req.body;
+
+    try {
+        const existingUser = await User.findOne({ 'user.id': user.referral_id });
+        if (!existingUser) {
+            res.status(200).send({ success: true, message: 'User not found' });
+            return;
+        }
+
+        res.status(200).send({ success: true, data: existingUser });
     } catch (error) {
         res.status(500).send({ success: false, message: error.message });
     }
@@ -61,9 +78,7 @@ export const getInvitedFriends = async (req, res) => {
             return res.status(200).send({ success: true, data: [], message: 'No invited friends found' });
         }
 
-        const invitedUsers = await User.find({
-            _id: { $in: user.invited_friends }
-        });
+        const invitedUsers = await User.find({ _id: { $in: user.invited_friends } });
 
         res.status(200).send({ success: true, data: invitedUsers });
     } catch (error) {
--- src/routes/bot.routes.js
+++ src/routes/bot.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, saveRefUser, getInvitedFriends } from '../controllers/bot.controller.js';
+import { register, saveReferal, getReferal, getInvitedFriends } from '../controllers/bot.controller.js';
 
 var router = express.Router();
 
 router.post("/register", register)
-router.post("/saveRefUser", saveRefUser)
+router.post("/saveReferal", saveReferal)
+router.post("/getReferal", getReferal)
 router.post("/getInvitedFriends", getInvitedFriends)
 
 export default router;
